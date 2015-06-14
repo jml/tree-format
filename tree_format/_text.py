8551f9f680408b45a93ee59b2dc45da0f2f50c38
Jonathan Lange <jml@mumak.net>
2015-06-14 09:12:53 +0100

    Implement print_tree correctly

--- tree_format/_text.py
+++ tree_format/_text.py
@@ -49,5 +49,5 @@ def format_tree(node, format_node, get_children):
     return u'\n'.join(lines)
 
 
-def print_tree():
-    print format_tree()
+def print_tree(node, format_node, get_children):
+    print format_tree(node, format_tree, get_children)
