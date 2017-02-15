3b3dd3c2033f6e44faba98cd2662f1f46d21b215
Jonathan Jacobs <jonathan@jsphere.com>
2017-02-15 15:02:31 +0200

    Format newlines so as not to disturb the tree rendering.

--- tree_format/tests/test_text.py
+++ tree_format/tests/test_text.py
@@ -107,6 +107,32 @@ class TestFormatTree(TestCase):
                 doctest.REPORT_NDIFF))
 
 
+    def test_newlines(self):
+        tree = (
+            'foo', [
+                ('bar\nfrob', [
+                    ('a', []),
+                    ('b\nc\nd', []),
+                ]),
+                ('baz', []),
+                ('qux\nfrab', []),
+            ],
+        )
+        output = self.format_tree(tree)
+        self.assertEqual(dedent(u'''\
+        foo
+        ├── bar⏎
+        │   frob
+        │   ├── a
+        │   └── b⏎
+        │       c⏎
+        │       d
+        ├── baz
+        └── qux⏎
+            frab
+        '''), output)
+
+
 def d(name, files):
     return (name, files)
 
